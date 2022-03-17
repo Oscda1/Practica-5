0152142da8deb9accba1d269a2e2ede4b7850793
Karsam <oscarbecerradevelop@gmail.com>
2022-03-17 00:36:19 -0700

    2.0

--- sistema/main/main.java
+++ sistema/main/main.java
@@ -5,7 +5,26 @@ class practica5{
     public static void main(String args[])
     {
         banco sistemas = new banco();
-        sistemas.Registrar();
-        sistemas.Ingreso();
+        boolean salir=false;
+        do{
+            try {
+                System.out.print("Que desea hacer?\n1)Registrar un nuevo usuario\n2)Iniciar sesion\n3)Salir\n");
+            switch(Integer.parseInt(System.console().readLine())){
+                case 1:
+                    sistemas.Registrar();
+                    break;
+                case 2:
+                    sistemas.Ingreso();
+                    break;
+                case 3:
+                    salir=true;
+                    break;
+                default:
+                    System.out.print("Ingrese una opcion valida\n");
+            }
+            } catch (Exception e) {
+                System.out.print("Ingrese un numero\n");
+            }
+        }while(salir==false);
     }
 }
